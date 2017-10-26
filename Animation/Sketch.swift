b038281e75e1c76017a9d8b4c1aaee7be851203e
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-10-26 11:06:52 -0400

    five circles -- complete!!!

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,27 +8,44 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var dx : Int
     
     // This function runs once
     override init() {
+        
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
         x = 250
+        dx = 250
+        
+        canvas.drawShapesWithBorders = false 
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        x += 2
+        dx += -2
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        // Draw an ellipse in the middle of the canvaS
+        canvas.fillColor = Color.green
+        canvas.drawEllipse(centreX: x , centreY: 250, width: 50, height: 50)
         
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX: dx, centreY: 150, width: 50, height: 50)
+        
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x , centreY: 50, width: 50, height: 50)
+        
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(centreX: x , centreY: 450, width: 50, height: 50)
+        
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: dx, centreY: 350, width: 50, height: 50)
     }
     
 }
