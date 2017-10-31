57832c609ee44c492be229e3b16c72264a612ffa
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-10-31 14:39:08 -0400

    still in progress; need to get the ball to move on the y axis too

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var dx : Int
     
     // This function runs once
     override init() {
@@ -16,18 +17,31 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+      dx = 2
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
         
+        // Change position
+        x += dx
+        
+        if x > 500 {
+            dx = -2
+        }
+     
+        if x < 0 {
+            dx = 2
+        }
+        
+        canvas.fillColor = Color.black
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x + 1, centreY: 250, width: 50, height: 50)
         
     }
     
